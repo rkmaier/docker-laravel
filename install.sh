f283f75490c5e9f3d5238b000a77bbb412002792
DESKTOP-0GQUEJV\Erik <rkmaier@gmail.com>
2019-01-16 19:42:58 +0100

    Init setup

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "INSTALL MISSING PACKAGES"
+cd /var/www > /dev/null 2>&1 &
+apt-get update -y
+apt-get install -y php7.0-curl
+apt-get install -y vim
+
+echo "COMPOSER INSTALL"
+composer install
+php artisan:migrate
+
+exec "/usr/bin/supervisord"

91c0f207149d216cab659fc104c12aab2df01d23
DESKTOP-0GQUEJV\Erik <rkmaier@gmail.com>
2019-01-16 22:13:44 +0100

    Updates

---
+++ docker/scripts/mariadb_init.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+/usr/bin/mysqld_safe > /dev/null 2>&1 &
+
+mysqladmin --silent --wait=30 ping
+mysql -uroot -e "CREATE USER 'erik'@'%' IDENTIFIED BY 'skipjack'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'erik'@'%' WITH GRANT OPTION"
+mysql -uroot -e "FLUSH PRIVILEGES"
+mysql -uroot -e "DROP DATABASE database_1 IF EXISTS"
+mysql -uroot -e "CREATE SCHEMA database_1 IF NOT EXISTS"
+
+echo "=> Done!"
+
+echo "========================================================================"
+echo "You can now connect to this MariaDB Server using:"
+echo ""
+echo "    mysql -uerik -pskipjack -h<host> -P<port>"
+echo ""
+echo "Please remember to change the above password as soon as possible!"
+echo "MariaDB user 'root' has no password but only allows local connections"
+echo "========================================================================"
+
+
+
+mysqladmin -uroot shutdown
--- start_cron.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/bin/php /var/www/artisan schedule:run >> /dev/null 2>&1
